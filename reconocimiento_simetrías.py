3e8b1587a881e8ff685f7befccbeccda4a3b592f
Dario Penagos <dariop@eva.local>
2025-12-13 12:13:03 -0500

    programando las simetrias

--- reconocimiento_simetrías.py
+++ reconocimiento_simetrías.py
@@ -38,6 +38,46 @@ def P0(f, X, n):
     X[:,n] *= -1
     return f(X)
 
+def H(f,X,t): return f(t*X)*t**(-X.shape[1])
+
+def CCY1(f,X,n,a):
+    tmp = X.clone()
+    X[:,n]+=a
+    tmp[:,n]=a
+    return f(X)-f(tmp)
+
+def CCY2(f,X,n,a):
+    tmp = X.clone()
+    X[:,n]*=a
+    tmp[:,n]=a
+    return f(X)/f(tmp)
+
+def CCY3(f,X,n,a):
+    tmp = X.clone()
+    X[:,n]+=a
+    tmp[:,n]=a
+    return f(X)/f(tmp)
+
+def CCY4(f,X,n,a):
+    tmp = X.clone()
+    X[:,n]*=a
+    tmp[:,n]=a
+    return f(X)-f(tmp)
+
+def T(f,X,n,a):
+    X[:,n]+=a
+    return f(X)
+
+def I(f,X, p): return f(X[:,p])
+
+def P(f,X,n,t):
+    X**=t
+    return f(X)/t
+
+def ESC(f,X,n,a):
+    X[:,n]*=a
+    return f(X)/a
+
 
 bs = 1028
 
@@ -61,15 +101,30 @@ for files in sim_df["Function"]:
     factors = factors.float()
     product = torch.from_numpy(f_dependent)
     product = product.float()
-    my_dataset = utils.TensorDataset(factors, product)
-    my_dataloader = utils.DataLoader(my_dataset, batch_size=bs, shuffle=True)
+    my_dataset = utils.TensorDataset(factors)
+    my_dataloader = utils.DataLoader(my_dataset, batch_size=len(my_dataset))
 
     model = SimpleNet(n_variables)
     model.load_state_dict(torch.load(f"NN_models/{files}/{file}")["model_state_dict"])
     model.eval()
 
     dist = nn.PairwiseDistance()
-    for x, y in my_dataloader:
-        print(dist(P1(model, x.clone(), 0), model(x)).mean())
-        print(dist(P0(model, x.clone(), 0), model(x)).mean())
-        break
+    for [x] in my_dataloader:
+        eval_dist = lambda a: dist(a, model(x))
+        
+        for f in [P0,P1]:
+            print(f"sim: {f.__name__}, dist: {eval_dist(f(model,x.clone(),0)).mean()}")
+        
+        for f in [H]:
+            print(f"sim: {f.__name__}, dist: {eval_dist(f(model,x.clone(),3)).mean()}")
+        
+        for f in [CCY1,CCY2,CCY3,CCY4,T,P,ESC]:
+            print(f"sim: {f.__name__}, dist: {eval_dist(f(model,x.clone(),0, 0.3)).mean()}")
+
+        for f in [I]:
+            print(f"sim: {f.__name__}, dist: {eval_dist(f(model,x.clone(),[0,1])).mean()}")
+        
+
+
+
+
